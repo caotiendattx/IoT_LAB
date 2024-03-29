a999ef97f6cb35b3e4b26ffdc823ab75bb4ade91
Tien Dat <caotiendattx@gmail.com>
2024-03-29 16:01:27 +0700

    add credentials

--- mqtt.py
+++ mqtt.py
@@ -2,7 +2,16 @@ import paho.mqtt.client as mqtt
 
 class MQTTHelper:
 
- 
+    MQTT_SERVER = "io.adafruit.com"
+    MQTT_PORT = 1883
+    MQTT_USERNAME = "caotiendattx"
+    MQTT_PASSWORD = "aio_MvFj45bGG8L3BWJtwVZISqY87kKl"
+
+    MQTT_TOPIC = "caotiendattx/feeds/bbc-led"
+    MQTT_TOPIC_TEMP = "caotiendattx/feeds/bbc-temp"
+    MQTT_TOPIC_HUMID = "caotiendattx/feeds/bbc-humid"
+    MQTT_TOPIC_CO2 = "caotiendattx/feeds/bbc-co2"
+    recvCallBack = None
 
     def mqtt_connected(self, client, userdata, flags, rc):
         print("Connected succesfully!!")
